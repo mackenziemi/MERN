62a97315be431de6950959da875bac649d7545fa
MacKenzie Mickelsen <mackenzie.mickelsen@churchofjesuschrist.org>
2022-08-11 17:16:45 -0600

    Added GetById, Update, and Delete functionality

--- src/backend/controllers/todos.controller.ts
+++ src/backend/controllers/todos.controller.ts
@@ -4,10 +4,35 @@ const getAllTodos = async () => {
     return await Todo.find({});
 }
 
+const getTodoById = async (id: string) => {
+    return await Todo.findById(id);
+}
+
 const createNewTodo = async (title: string, description: string) => {
     const todo = Todo.build({title, description})
     await todo.save();
     return todo;
 }
 
-export {getAllTodos, createNewTodo}
+const updateTodoById = async(id: string, title: string, description: string) => {
+    const update = {
+        'title': title, 
+        'description': description
+    };
+
+    await Todo.findByIdAndUpdate(id, update);
+    return true;
+}
+
+const deleteTodoById = async(id: string) => {
+    await Todo.findByIdAndDelete(id);
+    return true;
+}
+
+export {
+    getAllTodos, 
+    getTodoById, 
+    createNewTodo, 
+    updateTodoById, 
+    deleteTodoById
+}
--- src/backend/routes/todos.routes.ts
+++ src/backend/routes/todos.routes.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from 'express';
 import { Todo } from '../models/todo.model';
-import { getAllTodos, createNewTodo } from '../controllers/todos.controller';
+import { 
+    getAllTodos, 
+    getTodoById, 
+    createNewTodo,
+    updateTodoById,
+    deleteTodoById
+} from '../controllers/todos.controller';
 
 
 const router = express.Router();
@@ -10,12 +16,29 @@ router.get('/api/todos', async (req: Request, res: Response)=> {
     return res.status(200).send(todos);
 })
 
+router.get('/api/todo/:id', async (req: Request, res: Response)=> {
+    const id = req.params.id;
+    const todo = await getTodoById(id);
+    return res.status(200).send(todo);
+})
+
 router.post('/api/todo', async (req: Request, res: Response)=> {
     const { title, description } = req.body;
     const todo = await createNewTodo(title, description);
-    return res.status(201).send(todo);
+    return res.status(201).send(todo);    
+})
 
-    
+router.put('/api/todo/:id', async (req: Request, res: Response)=>{
+    const id = req.params.id;
+    const { title, description} = req.body;
+    const result = await updateTodoById(id, title, description);
+    return res.status(200).send(result);
+})
+
+router.delete('/api/todo/:id', async (req: Request, res: Response)=>{
+    const id = req.params.id;
+    const result = await deleteTodoById(id);
+    return res.status(200).send(result);
 })
 
 
